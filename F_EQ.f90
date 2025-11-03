5dc0836f98d331799b495cd94da34be3e31b20a0
Hamed Vaseghnia <93613536+hamedvaseghnia@users.noreply.github.com>
2025-11-03 08:35:05 +0100

    Update F_EQ.f90

--- F_EQ.f90
+++ F_EQ.f90
@@ -9,11 +9,11 @@ SUBROUTINE F_EQ
             DO X=1,XMAX
                 IF (WALL(X,Y,Z) .EQ. 0) THEN
                     
-                    USQR(X,Y,Z) = UREAL(X,Y,Z) * UREAL(X,Y,Z) + VREAL(X,Y,Z) * VREAL(X,Y,Z) + WREAL(X,Y,Z) * WREAL(X,Y,Z)
+                    USQR(X,Y,Z) = U(X,Y,Z) * U(X,Y,Z) + V(X,Y,Z) * V(X,Y,Z) + W(X,Y,Z) * W(X,Y,Z)
     
                     
                     DO I=0,18
-                        UXY(X,Y,Z) = UREAL(X,Y,Z)*E(I,0) + VREAL(X,Y,Z)*E(I,1) + WREAL(X,Y,Z)*E(I,2)
+                        UXY(X,Y,Z) = U(X,Y,Z)*E(I,0) + V(X,Y,Z)*E(I,1) + W(X,Y,Z)*E(I,2)
                         FEQ(I,X,Y,Z) = WF(I) * DXY(X,Y,Z) * (1.0d0 + 3.0d0*UXY(X,Y,Z) + 4.5d0*UXY(X,Y,Z)*UXY(X,Y,Z) - 1.5d0*USQR(X,Y,Z))
                     END DO
                 END IF
@@ -23,4 +23,5 @@ SUBROUTINE F_EQ
     !$OMP END PARALLEL DO
     
     END SUBROUTINE
+
     
